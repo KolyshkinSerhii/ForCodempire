b179471e297892d15b14945121014e6e49d506dc
KolyshkinSerhii <Sparxxx1986@gmail.com>
2021-06-08 18:27:52 +0300

    6.8.2021 update

--- src/Components/Statistic/Country.tsx
+++ src/Components/Statistic/Country.tsx
@@ -1,26 +1,96 @@
 import React, { useState } from "react";
 import { CountryType } from "../../Redux/Statistic";
 import s from "./Country.module.css";
-import { ModalWindow } from "./ModalWindow";
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import health from './/..//..//images/health.png'
+import death from './/..//..//images/death.png'
+import recovered from './//..//..//images/recover.png'
 
 type PropsType = {
     Country: CountryType;
 };
 
+const useStyles = makeStyles((theme) => ({
+    modal: {
+        display: 'grid',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    paper: {
+        width: '300px',
+        alignItems: 'center',
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+        borderRadius: '10px'
+    },
+    country: {
+        fontSize: '20px',
+        fontWeight: "bold",
+        textAlign: 'center',
+        paddingBottom: '20px',
+    },
+    info: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        textAlign: 'left',
+        padding: '15px'
+    },
+
+}));
+
 export const Country: React.FC<PropsType> = React.memo(({ Country }): JSX.Element => {
 
-    let [modalActive, SetModalActive] = useState(false)
+    let [open, setOpen] = useState(false)
+    const classes = useStyles();
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     return (
-        <div className={s.country}>
-            <button className={s.countryButton} onClick={() => {SetModalActive(true)}}>{Country.Country}</button>
-            <div>{Country.TotalDeaths}</div>
-            <ModalWindow active={modalActive} setActive={SetModalActive}>
-                <div>{Country.Country}</div>
-                <div>Total confirmed: {Country.TotalConfirmed}</div>
-                <div>Total deaths: {Country.TotalDeaths}</div>
-                <div>Total recovered: {Country.TotalRecovered}</div>
-            </ModalWindow>
+        <div>
+            <div className={s.country}>
+                <button className={s.country_bottom} onClick={handleOpen}>{Country.Country}</button>
+                <div className={s.total} >{Country.TotalConfirmed}</div>
+            </div>
+            <Modal className={classes.modal}
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}>
+                <Fade in={open}>
+                    <div className={classes.paper}>
+                        <div className={classes.country}>{Country.Country}</div>
+                        <div className={classes.info}>
+                            <img src={health} alt=''></img>
+                            <div>Total confirmed</div>
+                            <div>{Country.TotalConfirmed}</div>
+                        </div>
+                        <div className={classes.info}>
+                            <img src={death} alt=''></img>
+                            <div>Total deaths</div>
+                            <div>{Country.TotalDeaths}</div>
+                        </div>
+                        <div className={classes.info}>
+                            <img src={recovered} alt=''></img>
+                            <div>Total recovered</div>
+                            <div>{Country.TotalRecovered}</div>
+                        </div>
+                    </div>
+                </Fade>
+            </Modal>
         </div>
     );
 });
--- src/Components/Statistic/Statistic.tsx
+++ src/Components/Statistic/Statistic.tsx
@@ -4,6 +4,7 @@ import { getCountries } from "../../Redux/Selectors"
 import { requestCountries } from "../../Redux/Statistic"
 import { Country } from "./Country"
 import s from './Statistic.module.css'
+import CovidLogo from "./..//..//images/covid-logo.png"
 
 
 export const Statistic: React.FC = React.memo(() => {
@@ -26,25 +27,33 @@ export const Statistic: React.FC = React.memo(() => {
     const filteredCountries = useMemo(() => {
         if (!searchString) {
             return countries;
-        }   
+        }
         return countries.filter((c): any => c.Country.includes(searchString));
     }, [searchString, countries]);
 
     return (
         <div className={s.statistic}>
-            <div>
-                <div>COVID-19 STATISTIC</div>
-                <input type="text" placeholder="Search" onChange={handleChange}></input>
-            </div>
             <div className={s.header}>
-                <div>Countries</div>
-                <div>Total deaths</div>
+                <div className={s.wrapper}>
+                    <img className={s.logo} src={CovidLogo} alt="" />
+                    <div className={s.stat}>STATISTIC</div>
+                </div>
+                <div>
+                    <input className={s.search} type="text" placeholder="Search..." onChange={handleChange}></input>
+                </div>
             </div>
-            <div className={s.countries}>
-                {filteredCountries.map(c => <Country
-                    key={c.ID}
-                    Country={c} />)}
+            <div className={s.list}>
+                <div>№</div>
+                <div className={s.list_country} >Country</div>
+                <div className={s.list_total}>Total confirmed</div>
             </div>
+            <ol className={s.countries}>
+                {filteredCountries.map(c =>
+                    <li key={c.ID}>
+                        <Country
+                            Country={c} />
+                    </li>)}
+            </ol>
         </div>
     )
 })
